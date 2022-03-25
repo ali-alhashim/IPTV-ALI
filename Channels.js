189619d1bf143b70cb081f08528dd5bc92c7ee97
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-03-25 21:28:42 +0300

    first commit

---
+++ Channels.js
@@ -0,0 +1,613 @@
+var channelsMix = [
+
+
+
+
+
+
+
+
+
+  {
+    "Name": "قناة القمر",
+    "Server": "https://cdn5.iqsat.net/iq/8c17d37e0f5c88b1e9c7e1f8f82bc980.sdp/chunks.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+  {
+    "Name": "Bahrain",
+    "Server": "https://5c7b683162943.streamlock.net/live/ngrp:Bahrain%20TV_all/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+  {
+    "Name": "cartoon-net",
+    "Server": "https://shls-cartoon-net-prod-dub.shahid.net/out/v1/dc4aa87372374325a66be458f29eab0f/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "Al Sharqiya TV",
+    "Server": "https://5d94523502c2d.streamlock.net/home/mystream/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "alaantv",
+    "Server": "https://streamingcdn.alaan.tv/Publisher/alaantv_twitter/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+
+
+  {
+    "Name": "netTV",
+    "Server": "https://unlimited1-us.dps.live/nettv/nettv.smil/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+
+
+
+
+  {
+    "Name": "almamlka",
+    "Server": "https://almamlka-live.ercdn.net/almamlka/almamlka.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+  {
+    "Name": "TRT Arabic TV",
+    "Server": "https://5be5d840359c6.streamlock.net/afyonturktv/afyonturktv/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+
+
+  {
+    "Name": "AbuDhabi",
+    "Server": "https://admdn2.cdn.mangomolo.com/adtv/smil:adtv.stream.smil/chunklist_b4000000_t64MTA4MHA=.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "AbuDhabi Drama",
+    "Server": "https://admdn5.cdn.mangomolo.com/drama/smil:drama.stream.smil/chunklist_b1800000_t64NzIwcA==.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "alarabiya",
+    "Server": "https://live.alarabiya.net/alarabiapublish/alarabiya.smil/alarabiapublish/alarabiya_720p/chunks.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "syria tv",
+    "Server": "https://svs.itworkscdn.net/syriatvlive/syriatv.smil/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "alrayyan",
+    "Server": "https://svs.itworkscdn.net/alrayyanlive/alrayyan.smil/alrayyanpublish/alrayyan/chunks.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Rotana comedy",
+    "Server": "https://rotana.hibridmedia.com/rotana/comedy_dabr/rotana/comedy_1080p/chunks.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Rotana Drama",
+    "Server": "https://rotana.hibridmedia.com/rotana/drama_dabr/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "TRT Cocuk",
+    "Server": "https://tv-trtcocuk.live.trt.com.tr/master_720.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "womexerc",
+    "Server": "https://bozztv.com/36bay2/gin/giniko_womexerc_1000kb_36bay2/tracks-v1a1/mono.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+
+
+
+  {
+    "Name": "Ru Music",
+    "Server": "https://rutv.gcdn.co/streams/1410_1272/720n/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Russia 1",
+    "Server": "https://a3569457286-s26881.cdn.ngenix.net/hls/russia_hd/playlist_3.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+  {
+    "Name": "majid",
+    "Server": "https://admdn4.cdn.mangomolo.com/majid/smil:majid.stream.smil/playlist.m3u8?checkedby:hlscat.com",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+
+
+
+  {
+    "Name": "RT USA ",
+    "Server": "https://rt-usa.secure.footprint.net/1105_2500Kb.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Dubai one",
+    "Server": "https://dminnvll.cdn.mangomolo.com/dubaione/smil:dubaione.stream.smil/chunklist_b1300000.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "Sama Dubai",
+    "Server": "https://dmieigthvll.cdn.mangomolo.com/samadubai/smil:samadubai.stream.smil/chunklist_b1500000.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "Dubai",
+    "Server": "https://dmisxthvll.cdn.mangomolo.com/dubaitv/smil:dubaitv.stream.smil/chunklist_b1500000.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Spacetoon",
+    "Server": "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "DW News",
+    "Server": "https://dwstream4-lh.akamaihd.net/i/dwstream4_live@131329/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": " mbc action",
+    "Server": "https://shls-mbcaction-prod-dub.shahid.net/out/v1/68dd761538e5460096c42422199d050b/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": " MBC MAX HD",
+    "Server": "https://shls-mbcmax-prod-dub.shahid.net/out/v1/13815a7cda864c249a88c38e66a2e653/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": " mbc drama",
+    "Server": "https://shls-mbcdramaksa-prod-dub.shahid.net/out/v1/ce0f0762d89e4394a856c5fd13e43645/index_2.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc plus",
+    "Server": "https://shls-mbcplusdrama-prod-dub.shahid.net/out/v1/97ca0ce6fc6142f4b14c0a694af59eab/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc 5",
+    "Server": "https://shls-mbc5-prod-dub.shahid.net/out/v1/2720564b6a4641658fdfb6884b160da2/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc iraq",
+    "Server": "https://shls-iraq-prod-dub.shahid.net/out/v1/c9bf1e87ea66478bb20bc5c93c9d41ea/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc 1",
+    "Server": "https://shls-mbc1ksa-prod-dub.shahid.net/out/v1/451b666db1fb41c7a4bbecf7b4865107/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc 2",
+    "Server": "https://shls-mbc2-prod-dub.shahid.net/out/v1/b4befe19798745fe986f5a9bfba62126/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "mbc 4",
+    "Server": "https://shls-mbc4-prod-dub.shahid.net/out/v1/c08681f81775496ab4afa2bac7ae7638/index_1.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Sharjah",
+    "Server": "https://svs.itworkscdn.net/smc1live/smc1.smil/playlist_1080p.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "sky news",
+    "Server": "https://stream.skynewsarabia.com/hls/sna_720.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "AD National G",
+    "Server": "https://admdn2.cdn.mangomolo.com/nagtv/smil:nagtv.stream.smil/playlist.m3u8?adtv",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "NASA-NTV",
+    "Server": "https://ntv1.akamaized.net/hls/live/2014075/NASA-NTV1-HLS/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+  {
+    "Name": "alrai",
+    "Server": "https://stream02.fasttelco.net/live/alrai.stream/chunklist_w825183773.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "emarat",
+    "Server": "https://admdn3.cdn.mangomolo.com/emarat/smil:emarat.stream.smil/chunklist_b4000000_t64MTA4MHA=.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+  {
+    "Name": "Wanasah ",
+    "Server": "http://channelsstream.xyz:8080/live/streeem/streeem/13.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+  {
+    "Name": "taha kids",
+    "Server": "http://channelsstream.xyz:8080/live/streeem/streeem/62.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+  {
+    "Name": "future tv",
+    "Server": "http://futuretv.cdn.mangomolo.com/futuretv/smil:futuretv.smil/chunklist_b500000.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+
+
+
+  {
+    "Name": "RT ARAB",
+    "Server": "https://rt-arab.secure.footprint.net/1104_2500Kb.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "3ABN KIDS TV",
+    "Server": "https://moiptvhls-i.akamaihd.net/hls/live/652318/secure/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "ALJAZEERA UK",
+    "Server": "https://live-hls-web-aje.getaj.net/AJE/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "QVC",
+    "Server": "https://lsqvc1uscln-lh.akamaihd.net/i/lsqvc1uscln_01@809410/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "TRT WORD",
+    "Server": "https://tv-trtworld.live.trt.com.tr/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "TRT 1",
+    "Server": "https://tv-trt1.live.trt.com.tr/master_720.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "ULTO",
+    "Server": "http://ultotv.ru:8888/live/uhd/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "sc.id-tv Cinema",
+    "Server": "https://sc.id-tv.kz/Cinema_34_35.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "sc.id-tv domkino_hd",
+    "Server": "https://sc.id-tv.kz/domkino_hd_38_39.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Shot TV",
+    "Server": "https://ch.iptvmate.net/5e933c221b143b0b9d00807a.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Кино 1",
+    "Server": "http://v2.catcast.tv/content/38617/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "RenTV",
+    "Server": "http://sc.id-tv.kz/RenTV_36_37.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  
+
+  {
+    "Name": "Kronehit HD",
+    "Server": "https://bitcdn-kronehit.bitmovin.com/v2/hls/chunklist_b3128000.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  
+
+  
+
+
+  {
+    "Name": "Al Hurra",
+    "Server": "https://mbnhls-lh.akamaihd.net/i/MBN_1@118619/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "Red Bull TV",
+    "Server": "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+  {
+    "Name": "&privé HD",
+    "Server": "https://y5w8j4a9.ssl.hwcdn.net/andprivehd/tracks-v1a1/index.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "free"
+
+  },
+
+
+  {
+    "Name": "FTV",
+    "Server": "https://fash1043.cloudycdn.services/slive/_definst_/ftv_ftv_midnite_k1y_27049_midnite_secr_108_hls.smil/chunklist_b2800000_t64NzIwcA==.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "vip"
+
+  },
+
+
+
+  {
+    "Name": "JASMIN TV",
+    "Server": "https://bozztv.com/36bay2/giniko_jasmintv/tracks-v1a1/mono.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "vip"
+
+  },
+
+
+ 
+
+  {
+    "Name": "mexicotv",
+    "Server": "  https://59ec5453559f0.streamlock.net/mexicotv/smil:miamitvmexicoROKU/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "vip"
+
+  },
+
+
+  {
+    "Name": "xxi",
+    "Server": "http://51.68.141.106:8081/tv/tv-xxi/playlist.m3u8",
+    "type": "application/x-mpegURL",
+    "Category": "vip"
+
+  },
+
+
+
+];
+
+//###################################################### vip list #######################################
