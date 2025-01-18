50b48511fdf4e9f0cf0cdd41fb8d9332e1b3051d
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2025-01-18 17:33:03 +0300

    Update Channels.js

--- Channels.js
+++ Channels.js
@@ -1,25 +1,6 @@
+//####################################   ^__^ ################ I can see you ^_*
 var channelsMix = [
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
 
 
   {
@@ -30,12 +11,6 @@ var channelsMix = [
 
   },
 
- 
-
-
-
-
-
 
 
   {
@@ -45,27 +20,6 @@ var channelsMix = [
     "Category": "free"
 
   },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
 
 
 
@@ -88,44 +42,6 @@ var channelsMix = [
 
 
 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
- 
-
-
-
   {
     "Name": "Spacetoon",
     "Server": "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/playlist.m3u8",
@@ -134,19 +50,6 @@ var channelsMix = [
 
   },
 
- 
-
-
-
-
-
- 
-
- 
-
-
-
- 
 
   {
     "Name": "sky news",
@@ -156,36 +59,6 @@ var channelsMix = [
 
   },
 
-  
-
- 
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-  
-
   {
     "Name": "ALJAZEERA UK",
     "Server": "https://live-hls-web-aje.getaj.net/AJE/index.m3u8",
@@ -194,28 +67,6 @@ var channelsMix = [
 
   },
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
- 
- 
-
-
-
-  
-
   {
     "Name": "Kronehit HD",
     "Server": "https://bitcdn-kronehit.bitmovin.com/v2/hls/master.m3u8",
@@ -223,12 +74,6 @@ var channelsMix = [
     "Category": "free"
 
   },
-
-  
-
-  
-
-
 
 
   {
@@ -239,19 +84,137 @@ var channelsMix = [
 
   },
 
- 
+  {
+   "Name":"Bahrain",
+   "Server":"https://5c7b683162943.streamlock.net/live/ngrp:bahraintvmain_all/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+ {
+   "Name":"India News",
+   "Server":"https://pl-indiatvnews.akamaized.net/out/v1/db79179b608641ceaa5a4d0dd0dca8da/index.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+{
+   "Name":"Bahrain Sport2",
+   "Server":"https://5c7b683162943.streamlock.net/live/ngrp:bahrainsportstwo_all/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+{
+   "Name":"AL-RASHEED",
+   "Server":"https://media1.livaat.com/static/AL-RASHEED-HD/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+
+{
+   "Name":"AL-HURRA",
+   "Server":"https://mbnv-video-ingest.akamaized.net/hls/live/2038904/MBNV_ALHURRA_MAIN_HLS/merge.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+
+{
+   "Name":"almamlakatv",
+   "Server":"https://bcovlive-a.akamaihd.net/4109c7ba30fd4a44ad9afe917c67a8c8/eu-central-1/6415809151001/playlist_dvr.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+{
+   "Name":"ammantvhd",
+   "Server":"https://htvnksdtmy.erbvr.com/ammantvhd/ammantvhd.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+{
+   "Name":"roya-comedy",
+   "Server":"https://playlist.fasttvcdn.com/pl/toa2uuhhygheuly7xtuqrg/roya-comedy/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+
+{
+   "Name":"roya-kids-originals",
+   "Server":"https://playlist.fasttvcdn.com/pl/ptllxjd03j6g9oxxjdfapg/roya-kids-originals/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+
+{
+   "Name":"roya-adventures",
+   "Server":"https://playlist.fasttvcdn.com/pl/toa2uuhhygheuly7xtuqrg/roya-adventures/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+{
+   "Name":"tabekh-w-nafekh",
+   "Server":"https://playlist.fasttvcdn.com/pl/toa2uuhhygheuly7xtuqrg/tabekh-w-nafekh/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+{
+   "Name":"aghaniaghani",
+   "Server":"https://streaming-a-1035.cdn.nextologies.com/6zj8y5nemi8a32vyjjbkv0oqx/77pcakd8reik3g8qdqnexlkmq/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
 
 
 
 
 
+{
+   "Name":"futurelive",
+   "Server":"https://live.kwikmotion.com/futurelive/ftv.smil/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
+
+
+{
+   "Name":"qatartv",
+   "Server":"https://qatartv.akamaized.net/hls/live/2026573/qtv1/master.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
 
 
 
- 
+{
+   "Name":"alrayyan",
+   "Server":"https://alrayyancdn.vidgyor.com/pub-nooldraybinbdh/liveabr/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
 
- 
 
+{
+   "Name":"alhadath",
+   "Server":"https://live.alarabiya.net/alarabiapublish/alhadath.smil/playlist.m3u8",
+   "type": "application/x-mpegURL",
+   "Category": "free"
+  },
 
 
 
